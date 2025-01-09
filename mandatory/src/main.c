94de239b255d45392c97020ceecd493e4bca8bc9
togauthi <togauthier23@gmail.com>
2025-01-09 15:32:20 +0100

    Ask for command and free them (but do nothing)

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -3,15 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cmorel <cmorel@42angouleme.fr>             +#+  +:+       +#+        */
+/*   By: togauthi <togauthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 13:41:44 by cmorel            #+#    #+#             */
-/*   Updated: 2025/01/09 13:48:33 by cmorel           ###   ########.fr       */
+/*   Updated: 2025/01/09 15:27:38 by togauthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
+	char	*line;
+
+	(void)argc;
+	(void)argv;
+	while (1)
+	{
+		line = readline("\e[0;36m──(\e[0;33m/Documents/truc \e[1;32mSegfault)\e[0;36m──\e[1;36m> ");
+		if (!line)
+			break;
+		//He we should do something with the line
+		add_history(line);
+		free(line);
+	}
+	rl_clear_history();
 	return (0);
 }
