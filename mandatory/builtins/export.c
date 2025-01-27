ab5673e4d1030872ef00113e45c10f9ccb686151
togauthi <togauthier23@gmail.com>
2025-01-27 14:55:53 +0100

    Not really works

--- mandatory/builtins/export.c
+++ mandatory/builtins/export.c
@@ -1,32 +1,16 @@
 #include "../h_files/minishell.h"
 
-void	add_back(t_env **env, t_env *node);
-t_env	*sort_env(t_env **e);
+void	add_back(t_env **env, t_env *node, int append_mode);
+t_env	*sort_env(t_env **env);
 
 static char	*parse(char *line)
 {
-	int		i;
-	int		j;
-	int		skip;
-	char	*final;
+	int	i;
 
 	i = 6;
 	while (line[i] == ' ')
 		i++;
-	final = ft_calloc(ft_strlen(&line[i]) + 1, sizeof(char));
-	if (!final)
-		return (NULL);
-	j = i;
-	skip = 0;
-	while (line[j] && line[j] != ' ')
-	{
-		if (line[j] == '"')
-			skip++;
-		else
-			final[j - i - skip] = line[j];
-		j++;
-	}
-	return (final);
+	return (&line[i]);
 }
 
 void	*destroy_little(t_env *node)
@@ -41,65 +25,72 @@ void	*destroy_little(t_env *node)
 	return (NULL);
 }
 
-t_env	*new_env(char *str)
+t_env	*assign_node(char *str, int i, int append_mode, t_env **node)
+{
+	int		j;
+
+	(*node)->name = ft_substr(str, 0, i - append_mode);
+	j = i + 1;
+	while (str[j])
+		j++;	
+	(*node)->content = ft_substr(str, i + 1, j);;
+	if (!(*node)->name || !(*node)->content || ! (*node)->content[0])
+		return (destroy_little((*node)));
+	printf("Coucou");
+	return (*node);
+}
+
+t_env	*new_env(char *str, int *append_mode)
 {
 	t_env	*res;
 	int		i;
-	int		j;
 
 	i = 0;
+	*append_mode = 0;
 	res = ft_calloc(1, sizeof(t_env));
 	if (!res)
 		return (NULL);
 	while (str[i] != '=')
 	{
-		if (str[i] == '\0')
+		if (str[i] == '+' && str[i + 1] != '=')
 			return (destroy_little(res));
+		else if (str[i] == '+')
+			*append_mode = 1;
+		if (str[i] == '\0')
+			break;
 		i++;
 	}
-	res->name = ft_substr(str, 0, i);
-	j = i + 1;
-	while (str[j])
-		j++;
-	res->content = ft_substr(str, i + 1, j);
-	if (!res->name || !res->content || ! res->content[0])
-		return (destroy_little(res));
-	return (res);
+	return (assign_node(str, i, *append_mode, &res));
 }
 
-void	print_env(t_env **env)
-{
-	t_env	*node;
-
-	node = *env;
-	while (node)
-	{
-		printf("%s=%s\n", node->name, node->content);
-		node = node->next;
-	}
-}
-
+void	print_env(t_env **env);
+char	*parse_quotes(char *str, int multiple_args, t_env **env);
 void	*destroy(t_env *env);
 
 int	export(char *line, t_env **env)
 {
+	char	*parsed;
 	char	*final;
 	t_env	*node;
 	t_env	*sort;
+	int		append_mode;
 
-	final = parse(line);
-	if (!final)
+	parsed = parse(line);
+	if (!parsed)
 		return (1);
-	if (final[0] == '\0')
+	if (parsed[0] == '\0')
 	{
+
 		sort = sort_env(env);
 		print_env(&sort);
 		destroy(sort);
+		return (1);
 	}
-	node = new_env(final);
+	final = parse_quotes(parsed, 0, env);
+	node = new_env(final, &append_mode);
 	free(final);
 	if (!node)
 		return (1);
-	add_back(env, node);	
+	add_back(env, node, append_mode);
 	return (0);
 }
