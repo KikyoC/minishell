163f4214d11ac3feacd139858d368b9e0f362520
togauthi <togauthier23@gmail.com>
2025-01-28 17:38:19 +0100

    Now using the Clement's struct

--- mandatory/builtins/echo.c
+++ mandatory/builtins/echo.c
@@ -1,76 +1,37 @@
 #include "../h_files/minishell.h"
-#include <linux/limits.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-static char	*parse(char *str, int *new_line)
+char	*parse_quotes(char *str, int multile_args, t_env **env);
+
+void	write_echo(char **args, t_env **env)
 {
-	int	i;
-
-	i = 4;
-	while (str[i] == ' ')
-		i++;
-	if (str[i] == '-' && str[i + 1] == 'n')
-		*new_line = 0;
-	if (*new_line)
-		return (&str[i]);
-	i += 2;
-	while (str[i] == ' ')
-		i++;
-	return (&str[i]);
-}
-
-int	try_print_home(char *str)
-{
-	char	*home;
 	int		i;
+	char	*to_print;
 
-	if (str[0] == '~' && (str[1] == '\0' || str[1] == ' '))
+	i = 0;
+	while (args[i])
 	{
-		home = getenv("HOME");
-		i = -1;
-		if (home)	
-			while (home[++i])
-				write (1, &home[i], 1);
-		return (1);
-	}
-	return (0);
-}
-
-void	write_echo(char *str)
-{
-	char	in_quote;
-	int		i;
-
-	in_quote = 0;
-	i = -1;
-	while (str[++i])
-	{
-		if (try_print_home(&str[i]))
+		to_print = parse_quotes(args[i], 1, env);
+		if (!to_print)
 			continue ;
-		if (!in_quote && i > 1 && str[i] != ' ' && str[i - 1] == ' ')
-			write(1, " ", 1);
-		if (!in_quote && (str[i] == '"' || str[i] == '\''))
-			in_quote = str[i];
-		else if (in_quote && (str[i] == '"' || str[i] == '\''))
-			in_quote = 0;
-		else if (str[i] == ' ' && !in_quote)
-			continue ;
-		else if (str[i] == '\\' && str[i + 1] == '"')
-			write(1, &str[++i], 1);
-		else
-			write(1, &str[i], 1);
+		printf("%s", to_print);
+		i++;
+		if (args[i])
+			printf(" ");
 	}
 }
 
-void	echo(char *str)
+void	echo(t_list *lst, t_env **env)
 {
-	char	*final;
-	int		new_line;
+	int	new_line;
 
 	new_line = 1;
-	final = parse(str, &new_line);
-	write_echo(final);
+	if (ft_strncmp(lst->flags[0], "-n", 3) == 0)
+		new_line = 0;
 	if (new_line)
-		write(1, "\n", 1);
+	{
+		write_echo(lst->flags, env);
+		printf("\n");
+	}
+	else
+		write_echo(&lst->flags[1], env);
 }
--- mandatory/builtins/unset.c
+++ mandatory/builtins/unset.c
@@ -1,63 +1,38 @@
 #include "../h_files/minishell.h"
-#include <string.h>
 
-static char	*parse(char *line)
+void	remove_env(char *str, t_env **env)
 {
-	int		i;
-	int		j;
-	int		skip;
-	char	*final;
-
-	i = 5;
-	while (line[i] == ' ')
-		i++;
-	final = ft_calloc(ft_strlen(&line[i]) + 1, sizeof(char));
-	if (!final)
-		return (NULL);
-	j = i;
-	skip = 0;
-	while (line[j] && line[j] != ' ')
-	{
-		if (line[j] == '"')
-			skip++;
-		else
-			final[j - i - skip] = line[j];
-		j++;
-	}
-	return (final);
-}
-
-int	unset(char *line, t_env **env)
-{
-	char	*final;
 	t_env	*node;
-	t_env	*to_delete;
-	int		i;
+	size_t	len;
 
-	final = parse(line);
-	if (!final)
-		return (1);
 	node = *env;
-	i = ft_strlen(final);
-	to_delete = NULL;
+	len = ft_strlen(str);
 	while (node)
 	{
-		if (!node->next)
-			break ;
-		if (strncmp(final, node->next->name, i) == 0)
+		if (ft_strncmp(node->name, str, len) == 0)
 		{
-			to_delete = node->next;
-			node->next = to_delete->next;
-			break ;
+			if (node->prev)
+				node->prev->next = node->next;
+			if (node->next)
+				node->next->prev = node->prev;
+			free(node->name);
+			free(node->content);
+			free(node);
+			return ;
 		}
 		node = node->next;
 	}
-	if (to_delete)
+}
+
+int	unset(t_list *lst, t_env **env)
+{
+	int	i;
+
+	i = 0;
+	while (lst->flags[i])
 	{
-		free(to_delete->name);
-		free(to_delete->content);
-		free(to_delete);
+		remove_env(lst->flags[i], env);
+		i++;
 	}
-	free(final);
 	return (0);
 }
