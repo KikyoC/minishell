1b3faed8bbb18c7827cffd958ca600e8ffaec953
togauthi <togauthier23@gmail.com>
2025-01-28 16:59:36 +0100

    Now using the Clement's struct

--- mandatory/builtins/export.c
+++ mandatory/builtins/export.c
@@ -3,14 +3,28 @@
 void	add_back(t_env **env, t_env *node, int append_mode);
 t_env	*sort_env(t_env **env);
 
-static char	*parse(char *line)
+char	*parse_quotes(char *str, int multiple_args, t_env **env);
+
+static int	parse(char *str, int *append_mode)
 {
 	int	i;
+	int	part;
 
-	i = 6;
-	while (line[i] == ' ')
+	i = 0;
+	part = 0;
+	*append_mode = 0;
+	while (str[i])
+	{
+		if (str[i] == '+' && str[i + 1] == '=')
+		{
+			*append_mode = 1;
+			return (str[i + 2] != '\0');
+		}
+		else if (str[i] == '=')
+			return (str[i + 1] != '\0');
 		i++;
-	return (&line[i]);
+	}
+	return (1);
 }
 
 void	*destroy_little(t_env *node)
@@ -25,72 +39,85 @@ void	*destroy_little(t_env *node)
 	return (NULL);
 }
 
-t_env	*assign_node(char *str, int i, int append_mode, t_env **node)
+t_env	*assign_node(char *str, int append_mode, t_env **node)
 {
+	int		i;
 	int		j;
 
+	i = 0;
+	while (str[i] != '=')
+		i++;
 	(*node)->name = ft_substr(str, 0, i - append_mode);
 	j = i + 1;
 	while (str[j])
-		j++;	
-	(*node)->content = ft_substr(str, i + 1, j);;
+		j++;
+	(*node)->content = ft_substr(str, i + 1, j);
 	if (!(*node)->name || !(*node)->content || ! (*node)->content[0])
 		return (destroy_little((*node)));
-	printf("Coucou");
 	return (*node);
 }
 
-t_env	*new_env(char *str, int *append_mode)
+t_env	*new_env(char *str, int append_mode, t_env **env)
 {
 	t_env	*res;
 	int		i;
+	char	*tmp;
 
 	i = 0;
-	*append_mode = 0;
 	res = ft_calloc(1, sizeof(t_env));
 	if (!res)
 		return (NULL);
 	while (str[i] != '=')
-	{
-		if (str[i] == '+' && str[i + 1] != '=')
-			return (destroy_little(res));
-		else if (str[i] == '+')
-			*append_mode = 1;
-		if (str[i] == '\0')
-			break;
 		i++;
-	}
-	return (assign_node(str, i, *append_mode, &res));
+	res = assign_node(str, append_mode, &res);
+	tmp = parse_quotes(res->content, 0, env);
+	if (!tmp)
+		return (res);
+	free(res->content);
+	res->content = tmp;
+	return (res);
 }
 
-void	print_env(t_env **env);
-char	*parse_quotes(char *str, int multiple_args, t_env **env);
-void	*destroy(t_env *env);
-
-int	export(char *line, t_env **env)
+static void	print_env(char *str, t_env **env)
 {
-	char	*parsed;
-	char	*final;
 	t_env	*node;
-	t_env	*sort;
-	int		append_mode;
 
-	parsed = parse(line);
-	if (!parsed)
-		return (1);
-	if (parsed[0] == '\0')
+	node = *env;
+	while (node)
 	{
-
-		sort = sort_env(env);
-		print_env(&sort);
-		destroy(sort);
-		return (1);
+		if (ft_strncmp(node->name, str, ft_strlen(str)) == 0)
+		{
+			printf("%s=%s\n", node->name, node->content);
+			return ;
+		}
+		node = node->next;
 	}
-	final = parse_quotes(parsed, 0, env);
-	node = new_env(final, &append_mode);
-	free(final);
-	if (!node)
-		return (1);
-	add_back(env, node, append_mode);
-	return (0);
+	printf("NULL\n");
+}
+
+int	export(t_list *lst, t_env **env)
+{
+	int		i;
+	int		append_mode;
+	t_env	*node;
+	char	*s;
+
+	i = 0;
+	while (lst->flags[i])
+	{
+		if (!parse(lst->flags[i], &append_mode))
+		{
+			ft_putstr_fd("Minishell: ", 2);
+			ft_putstr_fd(lst->flags[i], 2);
+			ft_putstr_fd(" cannot be parsed\n", 2);
+		}
+		node = new_env(lst->flags[i], append_mode, env);
+		if (!node)
+			continue ;
+		s = ft_strdup(node->name);
+		add_back(env, node, append_mode);
+		print_env(s, env);
+		i++;
+	}
+	return (1);
 }
