6b9b4c889affe2573aaf01bc357729c2ff897a65
Tom GAUTHIER <tom@swordfun.fr>
2025-02-10 12:09:05 +0100

    Bon debut

--- mandatory/execution/execution.c
+++ mandatory/execution/execution.c
@@ -1,4 +1,5 @@
 #include "../h_files/minishell.h"
+#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -42,6 +43,9 @@ int	execute(t_list *cmd, char **env)
 	if (f == 0)
 	{
 		//Child process
+		printf("Input is %i\n", cmd->input);
+		dup2(cmd->input, 0);
+		// close(cmd->input);
 		execve(cmd->command, cmd->flags, env);
 		perror("Execution error\n");
 	}
@@ -50,39 +54,86 @@ int	execute(t_list *cmd, char **env)
 	return (f);
 }
 
-static void	add_back(int *lst, int to_add)
-{
-	int	i;
+// static void	add_back(int *lst, int to_add)
+// {
+// 	int	i;
+//
+// 	i = 0;
+// 	if (!lst)
+// 		return ;
+// 	while (lst[i] == -1)
+// 		i++;
+// 	lst[i] = to_add;
+// }
 
-	i = 0;
-	if (!lst)
-		return ;
-	while (lst[i] == -1)
-		i++;
-	lst[i] = to_add;
+// Actually it can handle < and >
+int	open_file(t_list *node, int *infile, int *outfile)
+{
+	if (ft_strncmp("<", node->prev->command, 2))
+	{
+		close(*infile);
+		*infile = open(node->command, O_RDONLY);
+		if (*infile < 0)
+		{
+			*infile = 0;
+			perror("Minishell: ");
+			return (1);
+		}
+		return (0);
+	}
+	if (ft_strncmp(">", node->prev->command, 2))
+	{
+		close(*outfile);
+		*outfile = open (node->command, O_WRONLY | O_TRUNC | O_CREAT);
+		if (*outfile < 0)
+		{
+			*outfile = 1;
+			perror("Minishell: ");
+			return (1);
+		}
+		return (0);
+	}
+	return (1);
+}
+
+t_list	*prepare_command(t_list	*node)
+{
+	int		infile;
+	int		outfile;
+	t_list	*command;
+
+	infile = 0;
+	outfile = 1;
+	command = NULL;
+	while (node)
+	{
+		// If it's a file
+		if (node->type == 4)
+		{
+			printf("It's a file\n");
+			open_file(node, &infile, &outfile);
+		}
+		else if (node->type == 1)
+			command = node;
+		node = node->next;
+	}
+	command->input = open("infile", O_RDONLY);
+	printf("Input is %i\n", command->input);
+	command->output = outfile;
+	return (command);
 }
 
 int	run(t_list **lst, t_env **env)
 {
-	t_list	*node;
-	int		*pids;
+	t_list	*command;
+	char	**envp;
+	int		state;
 
-	init(lst, env);
-	node = *lst;
-	pids = get_pid_list(*lst);
-	while (node)
-	{
-		if (get_builtin(node) == NULL)
-		{
-			//Execute a normal command
-			add_back(pids, execute(node, get_envp(env)));
-		}
-		else if (node->type == 1)
-		{
-			//Execute a builtin command
-			add_back(pids, builtin(node, env, get_builtin(node)));
-		}
-		node = node->next;
-	}
-	return (wait_all(pids));
+	command = prepare_command(*lst);
+	envp = get_envp(env);
+	(void)command;
+	(void)envp;
+	(void)state;
+	// waitpid(execute(command, envp), &state, 0);
+	return (1);
 }
