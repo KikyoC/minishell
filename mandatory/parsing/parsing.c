aed6ca73bd3d2e89af730cbd1bc6170012b73f3b
togauthi <togauthier23@gmail.com>
2025-02-18 16:51:18 +0100

    Added a fucntion to check if command is valid

--- mandatory/parsing/parsing.c
+++ mandatory/parsing/parsing.c
@@ -15,6 +15,32 @@ char	*expand(char *line, t_env *env)
 	return (new_line);
 }
 
+static int	check_commands(t_list *cmds)
+{
+	t_list	*node;
+	int		cmd_found;
+
+	node = cmds;
+	cmd_found = 0;
+	while (node)
+	{
+		if (node->type == 5 && (!node->next || node->next->type != 3))
+			return (0);
+		else if (node->type == 3 && (!node->prev || node->prev->type != 5))
+			return (0);	
+		else if (node->type == 2 && (!cmd_found || !node->next))
+			return (0);
+		else if (node->type == 1 && cmd_found)
+			return (0);
+		else if (node->type == 1)
+			cmd_found ++;
+		else if(node->type == 2)
+			cmd_found = 0;
+		node = node->next;
+	}
+	return (1);
+}
+
 t_list	*get_commands(char *line, t_env *env)
 {
 	t_list	*cmds;
@@ -29,5 +55,9 @@ t_list	*get_commands(char *line, t_env *env)
 	if (!cmds)
 		return (NULL);
 	get_correct_commands(cmds, env);
-	return (cmds);
+	if (check_commands(cmds))
+		return (cmds);
+	ft_putstr_fd("Invalid line\n", 2);
+	ft_lstclear(&cmds, free);
+	return (NULL);
 }
