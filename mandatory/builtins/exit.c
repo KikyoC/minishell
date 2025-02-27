2cc38051ec9f8bbaa8c12dfaab218591f04d1662
togauthi <togauthier23@gmail.com>
2025-02-27 15:00:56 +0100

    Now using real exit code

--- mandatory/builtins/exit.c
+++ mandatory/builtins/exit.c
@@ -1,40 +1,55 @@
 #include "../h_files/minishell.h"
 
-static int	parse_end(int code, char *code_origin)
+static int parse(unsigned long long *res, char *str)
 {
-	char	*str;
-	int		res;
+	int			i;
+	long long	tmp;
 
-	str = ft_itoa(code);
+	i = 0;
 	if (!str)
+	{
+		*res = 2;
+		return (1);
+	}
+	if (!ft_isdigit(str[0]) && str[0] != '+' && str[0] != '-')
 		return (0);
-	res = ft_strncmp(str, code_origin, ft_strlen(code_origin)) == 0;
-	free(str);
+	while (str[++i])
+		if (!ft_isdigit(str[i]))
+			return (0);
+	tmp = ft_atoi(str);
+	if ((tmp == -1) && str[1])
+		return (0);
+	*res = tmp;
 	return (1);
+}
+
+void	transform_code(unsigned long long *code)
+{
+	while (*code > 255)
+		*code %= 256;
 }
 
 int	exit_builtin(t_list *cmd, t_env **env)
 {
-	int		code;
-	t_env	*node;
+	unsigned long long		code;
 
 	code = 0;
-	if (cmd->flags && cmd->flags[0])
+	if (cmd->flags)
 	{
-		code = ft_atoi(cmd->flags[0]);
-		if (!parse_end(code, cmd->flags[0]))
-			code = 2;
+		if (cmd->flags[1])
+		{
+			ft_putstr_fd("Exit: Too many arguments\n", 2);
+			exit_code(1, env, 0, NULL);
+			return (1);
+		}
+		if (!parse(&code, cmd->flags[0]))
+		{
+			ft_putstr_fd("Exit: not valid number\n", 2);
+			exit_code(2, env, 0, NULL);
+			return (-2);
+		}
 	}
-	node = ft_calloc(1, sizeof(t_env));
-	if (!node)
-		return (-2);
-	node->name = ft_strdup("?");
-	node->content = ft_itoa(code);
-	if (!node->name || !node->content)
-	{
-		destroy_node(node);
-		return (-2);
-	}
-	add_back(env, node, 0);
+	transform_code(&code);
+	exit_code(code, env, 0, NULL);
 	return (-2);
 }
