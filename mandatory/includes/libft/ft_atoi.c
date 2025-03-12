619be71801e95c09b1fce21372b1ff6a6e1fa0b7
togauthi <togauthier23@gmail.com>
2025-03-12 10:16:04 +0100

    Real atoi

--- mandatory/includes/libft/ft_atoi.c
+++ mandatory/includes/libft/ft_atoi.c
@@ -1,27 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: togauthi <togauthi@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 09:17:28 by togauthi          #+#    #+#             */
+/*   Updated: 2025/03/12 09:17:36 by togauthi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	sign;
-	int	nbr;
+	int		p;
+	int		i;
+	long	res;
 
+	p = 1;
 	i = 0;
-	sign = 1;
-	nbr = 0;
-	if (!nptr)
-		return (0);
+	res = 0;
 	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || nptr[i] == ' ')
 		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign *= -1;
-		i++;
-	}
+	if (nptr[i] == '+' || nptr[i] == '-')
+		if (nptr[i++] == '-')
+			p = -1;
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		nbr = 10 * nbr + (nptr[i] - '0');
-		i++;
+		if (res > (LLONG_MAX - (nptr[i] - '0')) / 10)
+		{
+			if (p == 1)
+				return (-1);
+			else
+				return (0);
+		}
+		res = res * 10 + (nptr[i++] - '0');
 	}
-	return (nbr * sign);
+	return ((int)(res * p));
 }
